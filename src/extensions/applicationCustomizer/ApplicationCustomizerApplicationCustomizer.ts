68ab372043f668092be5ed4440fc2ee58d5689ee
Jason Alanya <jasonalanya853@gmail.com>
2023-07-17 23:51:35 -0500

    Fixing selector

--- src/extensions/applicationCustomizer/ApplicationCustomizerApplicationCustomizer.ts
+++ src/extensions/applicationCustomizer/ApplicationCustomizerApplicationCustomizer.ts
@@ -1,7 +1,5 @@
 //import { Log } from '@microsoft/sp-core-library';
-import {
-  BaseApplicationCustomizer
-} from '@microsoft/sp-application-base';
+import { BaseApplicationCustomizer } from "@microsoft/sp-application-base";
 //import { Dialog } from '@microsoft/sp-dialog';
 
 //import * as strings from 'ApplicationCustomizerApplicationCustomizerStrings';
@@ -19,37 +17,43 @@ export interface IApplicationCustomizerApplicationCustomizerProperties {
 }
 
 /** A Custom Action which can be run during execution of a Client Side Application */
-export default class ApplicationCustomizerApplicationCustomizer
-  extends BaseApplicationCustomizer<IApplicationCustomizerApplicationCustomizerProperties> {
-
+export default class ApplicationCustomizerApplicationCustomizer extends BaseApplicationCustomizer<IApplicationCustomizerApplicationCustomizerProperties> {
   public onInit(): Promise<void> {
-    console.log("funcionando")
-
     const applyStyles = (): void => {
-      const styles = `
+      const getNode = document.querySelectorAll(".Canvas");
+
+      if (getNode[0].children.length === 2) {
+        const childrens = [];
+        const children = getNode[0].children;
+        for (let i = 0; i < children.length; i++) {
+          childrens.push(children[i].className);
+        }
+        childrens.sort();
+
+        const styles = `
         @media screen and (min-width: 720px) {
-          .a_a_50a7110f.b_a_50a7110f{
+          .Canvas{
             display:flex
           }
-          .a_a_50a7110f.b_a_50a7110f .e_a_50a7110f {
+          .Canvas .${childrens[0]} {
             width: 75%;
             order: 1;
           }
-          .a_a_50a7110f.b_a_50a7110f .f_a_50a7110f {
+          .Canvas .${childrens[1]} {
             width: 25%;
             order: 2;
           }
         }
       `;
-    
-      const styleElement = document.createElement('style');
-      styleElement.innerHTML = styles;
-      document.head.appendChild(styleElement);
+
+        const styleElement = document.createElement("style");
+        styleElement.innerHTML = styles;
+        document.head.appendChild(styleElement);
+      }
     };
-    
+
     applyStyles();
-    
-    
+
     return Promise.resolve();
   }
 }
